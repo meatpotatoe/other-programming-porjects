fac3193b904cfae79b856b658b8db25e97437fce
meatpotatoe <evan_a_s2@yahoo.com>
2015-06-04 20:42:22 -0700

    Fixing formatting in problem 1

--- problem1.c
+++ problem1.c
@@ -1,11 +1,12 @@
-// project euler find the multipules of 3 and 5 and add all the results from 1-1000
-#include <iostream>;
-using namespacem std;
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+#include <iostream>
+using namespace std;
 int main()
 {
-// 3 & 5 to 1000 add
+    // 3 & 5 to 1000 add
 
-
-system("pause");
-return 0;
+    return 0;
 }
