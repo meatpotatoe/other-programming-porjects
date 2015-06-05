b5959edb7a7654f29cb71bb02fdc60a5f92d51df
russianfool <russianfool20825@gmail.com>
2015-06-05 01:39:29 -0700

    Changing the name of Evan's problem1.c to problem1.cpp since it uses elements of c++, and should be compiled with g++; at the same time, it should probably stick to one or the other, and if we're using C++, we have access to great STL libraries.

---
+++ problem1.cpp
@@ -0,0 +1,29 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+		int arr[1000];
+		int j = 0;
+		long long int sum = 0;
+		// 3 & 5 to 1000 add
+		cout << "we are running";
+		for(int i = 1; i< 1000; i++)
+		{
+				if (i%3 == 0 || i%5 == 0){
+						arr[j] = i;
+						j++;
+				}
+		}
+		cout << "\n";
+		for(int i = 0; i< j; i++)
+		{
+				sum += arr[i];
+		}
+		cout << sum << "\n";
+
+		return 0;
+}
