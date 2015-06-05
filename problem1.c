125691ba397d7fbbdf86ee4f637a6c71922d1b4e
meatpotatoe <evan_a_s2@yahoo.com>
2015-06-04 21:30:00 -0700

    this works

--- problem1.c
+++ problem1.c
@@ -7,21 +7,23 @@ using namespace std;
 int main()
 {
     int arr[1000];
-    int j = 0, sum = 0;
+    int j = 0;
+    long long int sum = 0;
    // 3 & 5 to 1000 add
     cout << "we are running";
     for(int i = 1; i< 1000; i++)
         {
-            if (i/3 == 0 || i/5 == 0){
+            if (i%3 == 0 || i%5 == 0){
                 arr[j] = i;
                 j++;
              }
-            for (int x; x<1000; x++)
-            {
-                sum += arr[x] 
-            }
         }
-    cout << sum;
+cout << "\n";
+    for(int i = 0; i< j; i++)
+        {
+            sum += arr[i];
+        }
+    cout << sum << "\n";
 
     return 0;
 }
