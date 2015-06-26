5c52fba41383a494d8b9d39d24612ffcc4da8b1e
meatpotatoe <evan_a_s2@yahoo.com>
2015-06-25 17:13:50 -0700

    solution to problem 2

--- problem2.cpp
+++ problem2.cpp
@@ -2,30 +2,29 @@
 using namespace std;
 int main()
 {
-long long int arr1[31], arr2[31];
-arr1[0] = 1;
-arr1[1] = 2;
+long long int arr1[31] = {1,2};
+long long int arr2[31];
 int count = 0;
-for(int i=0; i< 31; i++){
-    while(arr1[i]< 4000000){
-		cout << "My inner loop passed check, arr[i] is at " << arr1[i] << " and i = " << i << endl;
-        arr1[2+i] = arr1[i] + arr1[++i];
-		cout << "I've generated a new number, i is now " << arr1[i] << endl;
-        cout << arr1[i]<< "\n";
-    }
-	cout << "I've broken out of the inner loop, my i is at " << i << " right now" << endl;
+for(int i=0; arr1[2+i] < 4000000 ; i++){
+	cout << "My inner loop passed check, arr[i] is " << arr1[i] << " and i = " << i << endl;
+    cout << "now adding " << arr1[i] << " to " << arr1[1+i] << " into the element " << arr1[2+i] << endl;
+    arr1[2+i] = arr1[i] + arr1[1+i];
+	cout << "I've generated a new number, i is now " << arr1[2+i] << endl;
+    count++;
 }
-/*
-int j=0,sum;
-for(int i = 0; i < 50; i++){
+cout << "I'm out of the loop and my i is " << count << endl;
+
+int j=0,sum = 0;
+for(int i = 0; i < 2+count; i++){
+    cout << "this number is being tested " << arr1[i] << endl;
     if(arr1[i]%2==0){
         arr2[j] = arr1[i];
+        cout << "this is the number that is going into the second array " << arr2[j] << endl;
+        sum += arr2[j];
+        cout << "the sum is " << sum << endl;
         j++;
     }
 }
-for(int i = 0; i< 10; i++){
-sum += arr2[i];
-}
-cout << sum;*/
+cout << "this is the sum of the array " << sum << endl;
 return 0;
 }
