a63608553290c3b465c40b3517856b58dfe15612
meatpotatoe <evan_a_s2@yahoo.com>
2015-07-10 08:27:04 +0800

    my attempts

--- problem2.cpp
+++ problem2.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-    long long int arr1[31] = {1,2};
-    long long int arr2[31];
+    long long int arr1[32] = {1,2};
+    long long int arr2[32];
     int count = 0;
 
     for (int i = 0; arr1[2+i] < 4000000 ; i++) {
@@ -18,14 +18,17 @@ int main()
     int j=0, sum = 0;
     for (int i = 0; i < count + 2; i++) {
 
-        cout << "Testing: " << arr1[i] << "%2 == 0" << endl;
+        cout << "Testing: " << arr1[i] << "%2 == 0";
 
         if (arr1[i]%2 == 0) {
+            cout << " yup " << endl;
             arr2[j] = arr1[i];
             cout << "arr2[" << j << "] = " << arr2[j] << endl;
             sum += arr2[j];
             cout << "Sum is now: " << sum << endl;
             j++;
+        }else{
+            cout << " nope " << endl;
         }
     }
     cout << "Final sum: " << sum << endl;
